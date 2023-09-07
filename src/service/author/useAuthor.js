5521f6564f8fbdad4532c09b297b6bcef08cd141
Davranbek <davranbek_1995@mail.ru>
2023-09-07 09:21:38 +0500

    added api

---
+++ src/service/author/useAuthor.js
@@ -0,0 +1,19 @@
+
+
+
+import api from "../axios";
+
+const headers = {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer  ${localStorage.getItem("token")}`
+}
+
+const useAuthor = {
+    createAuthor: (data) => api.post("/author", data),
+    getAuthor: () => api.get("/author", {headers}),
+    getAuthorItem: (id) => api.get(`/author/${id}`, {headers}),
+    updateAuthor: (id,data) => api.put(`/author/${id}`, data),
+    deleteAuthor: (id) => api.delete(`/author/${id}`, {headers}),
+}
+
+export default useAuthor;
