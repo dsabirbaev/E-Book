2eb104d7e69bbf944efbb5ccf7c17611a350dfde
Davranbek <davran9559@gmail.com>
2023-09-16 22:57:43 +0500

    paginate with api

--- src/pages/Forum/index.jsx
+++ src/pages/Forum/index.jsx
@@ -3,10 +3,12 @@
 
 import { useState, useEffect } from "react";
 import PaginateTable from "./PaginateTable";
+import PaginateAPI from "./PaginateAPI";
 
 const index = () => {
 
     const [post, setPost] = useState([]);
+    const [user, setUser] = useState([]);
     const [load, setLoad] = useState(false);
 
     const [currentPage, setCurrentPage] = useState(1);
@@ -27,18 +29,33 @@ const index = () => {
         }
     }
 
-    const paginate = (index) => {
-        setCurrentPage(index)
+   
+    const getUsers = async(page) => {
+        try{
+            let response = await fetch(`https://reqres.in/api/users?page=${page}`);
+            let res = await response.json();
+            setUser(res);
+        }catch(err){
+            console.log(err.message)
+        }
     }
+
+    const paginate = (index) => {
+        // setCurrentPage(index)
+        getUsers(index)
+    }
+
 
     useEffect(() => {
         getPosts();
+        getUsers();
     }, [])
     return (
         <div className="shadow p-2 border m-4">
           
             <h1 className="text-center"> POSTS </h1>
-            <PaginateTable paginate={paginate} data={lastData} fullData = {post.length}/>
+            {/* <PaginateTable paginate={paginate} data={lastData} fullData = {post.length}/> */}
+            <PaginateAPI data={user} paginate={paginate}/>
         </div>
     );
 };
