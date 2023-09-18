0f87340266057a628fb1ef31d8e9c392bfc7640d
Davranbek <davran9559@gmail.com>
2023-09-19 01:40:57 +0500

    add category

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,8 +1,39 @@
-
+import { useEffect, useReducer } from "react";
 import CardAuthor from "../UI/Card/Author/CardAuthor";
-import CardBook from "../UI/Card/Book/CardBook";
+import useCategory from "../../service/category/useCategory";
 
-const Category = ({t}) => {
+const Category = ({ t }) => {
+
+    const initState = {
+        categoryList: []
+    }
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "SET_CATEGORY":
+                return { ...state, categoryList: action.payload };
+            default:
+                return state;
+        }
+    }
+
+    const [{ categoryList }, dispatch] = useReducer(reducer, initState);
+
+    const getCategory = async () => {
+        try {
+            const res = await useCategory.getCategory();
+            console.log(res.data)
+            if (res.status === 200) {
+                dispatch({ type: "SET_CATEGORY", payload: res.data })
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    useEffect(() => {
+        getCategory();
+    }, [])
+
     return (
         <section className='py-[50px]'>
             <div className='container'>
@@ -10,37 +41,27 @@ const Category = ({t}) => {
                     <h2 className='text-[#C9AC8C] text-center uppercase text-[31px] font-["Rotter"] mb-5'>{t?.mainCategories}</h2>
                     <nav className='mb-10'>
                         <ul className='flex items-center justify-center gap-x-[49px] text-[20px] text-gray-400'>
-                            <li className='p-[10px]'>
-                                <a href="#">Temuriylar davri </a>
-                            </li>
-                            <li className='p-[10px]'>
-                                <a href="#">Jadid adabiyoti  </a>
-                            </li>
-                            <li className='p-[10px]'>
-                                <a href="#">Sovet davri  </a>
-                            </li>
-                            <li className='p-[10px]'>
-                                <a href="#">Mustaqillik davri </a>
-                            </li>
+
+                            {
+                                categoryList.length ? categoryList.map((item) => {
+                                    return <li key={item.id} className='p-[10px] cursor-pointer p-1 rounded-md hover:bg-blue-100 hover:text-black'>
+                                              {item?.name} 
+                                            </li>
+                                }): null
+                            }
+                         
                         </ul>
                     </nav>
 
                     <div className="flex items-center justify-center flex-wrap gap-x-[37.22px] gap-y-[43.85px]">
                         {
-                            "HELLOWORLD!".split("").map((item) => {
-                                return <CardAuthor/>
+                            "HELLOWORLD!".split("").map((item, index) => {
+                                return <CardAuthor key={index} />
                             })
                         }
-                        
+
                     </div>
 
-                    {/* <div className="grid grid-cols-7 gap-x-[35px] gap-y-[52px]">
-                        {
-                            "HELLOWORLD!HEY".split("").map((item) => {
-                                return <CardBook/>
-                            })
-                        }
-                    </div> */}
                 </div>
             </div>
         </section>
